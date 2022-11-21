ad4d3841c064630027941c2d7bb79464b9befcc8
Phạm Hoàng <hoang1482002@gmail.com>
2022-11-21 22:07:06 +0700

    fix stupid black

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from ChessGame import ChessGame
 
 if __name__ == "__main__":
     game = ChessGame(
-        player1_is_human=False, player2_is_human=False, ai_level=5, game_speed=10
+        player1_is_human=False, player2_is_human=False, ai_level=3, game_speed=10
     )
     game.start()
